6acfab192ed3bece341d90d1a798e7384908f875
akash1998 <akash.online98@gmail.com>
2024-01-13 23:18:15 +0530

    Web Api initial

---
+++ Zexpo.Api/Controllers/ApiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Zexpo.Api.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ActionName("GetMethod")]
+        public string Getmethod()
+        {
+           _logger.LogInformation("Site hit today at {DT}", DateTime.UtcNow.ToString());
+            return "GET";
+        }
+        [HttpPost]
+        [ActionName("PostMethod")]
+        public string Postmethod(string name) { return "Hello " + name;}
+    }
+}
---
+++ Zexpo.Api/Program.cs
@@ -0,0 +1,30 @@
+namespace Zexpo.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
